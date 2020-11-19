12a76d834fbfef1e8ed57b0cb0873a958ba40a2c
chrisjfranko <chrisjfranko@gmail.com>
2020-11-19 09:18:04 -0500

    make exp changes

--- index.js
+++ index.js
@@ -1,33 +1,32 @@
 #!/usr/bin/env node
+require('dotenv').config()
 
 const ethers = require('ethers');
 const http = require('http');
 
 const host = process.env.RPC_HOST || 'localhost';
-const provider = ethers.getDefaultProvider(process.env.NETWORK);
-const localProvider = new ethers.providers.JsonRpcProvider(`http://${host}:8545`);
-const MAX_BLOCK_DIFFERENCE = 3;
+//const provider = ethers.getDefaultProvider(process.env.NETWORK);
+const localProvider = new ethers.providers.JsonRpcProvider(`http://${host}:9656`);
+const MIN_BLOCK = 4100000;
 
 const onHealthcheckRequest = async (req, res) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 
   let localBlockNum;
-  let networkBlockNum;
+  //let networkBlockNum;
 
   try {
     localBlockNum = await localProvider.getBlockNumber();
-    networkBlockNum = await provider.getBlockNumber();
+    //networkBlockNum = await provider.getBlockNumber();
   } catch (e) {
     console.error(e);
     res.writeHead(500, { 'Content-Type': 'text/plain' });
     res.end(e);
   }
 
-  let responseStatus = networkBlockNum - localBlockNum > MAX_BLOCK_DIFFERENCE ? 500 : 200;
-  if (localBlockNum > 10000 && networkBlockNum <= 0) { // don't let etherscan f**k us
-    responseStatus = 200;
-  }
+  let responseStatus = localBlockNum > MIN_BLOCK ? 500 : 200;
+
   res.writeHead(responseStatus, { 'Content-Type': 'text/plain' });
   res.end((localBlockNum - networkBlockNum).toString());
 };
